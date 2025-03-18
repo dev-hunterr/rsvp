2502702c201c7d29ca96dd617c07e0cbbe5d4af9
dev-hunterr <ogbanufavour@gmail.com>
2025-03-18 02:42:42 +0100

    feat(launch) - setup Supabase server, as well as the  server actions for the auth  and crud logic, setup middleware, built out interface and integrated the ui with the server actions

---
+++ lib/constants.ts
@@ -0,0 +1,20 @@
+export const strings = {
+  title: "Birthdaay RSVP🥳",
+  description:
+    "HEYY! Super Excited to share my special day with you. Please RSVP by Feb 14th, 2026. Let me know if you can make it.",
+  eventDate: "2026-02-14", // YYYY-MM-DD format
+  eventDateLabel: "Event Date",
+  eventLocation: "National Stadium Surulere Lagos, Lagos 101241",
+  eventLocationLabel: "Event Location",
+  nameLabel: "Full Name",
+  emailLabel: "Email Address",
+  accompanyLabel: "Number of Guests",
+  rsvpLabel: "Will you attend?",
+  yesOption: "Yes, I'll be there!",
+  noOption: "Sorry, I can't make it",
+  submitButton: "Send RSVP",
+  thankYouMessage: "Thank you for your response!",
+  viewOnMapButton: "View Location on Google Maps",
+  sendFromEmail: "onboarding@resend.dev",
+  sendToEmail: process.env.EMAIL_TO,
+};
