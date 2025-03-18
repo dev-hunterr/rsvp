68eff936c8a217857d818902db92462ae50e0576
dev-hunterr <ogbanufavour@gmail.com>
2025-03-18 09:06:02 +0100

    feat(emails) - integrated resend to automate sending email to admin when a reservation is made

--- app/actions/submitRSVP.ts
+++ app/actions/submitRSVP.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { strings } from "@/lib/constants";
 import { createClient } from "@/utils/supabase/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function submitRSVP(formData: FormData) {
   const supabase = await createClient();
@@ -18,6 +22,30 @@ export async function submitRSVP(formData: FormData) {
   if (error) {
     console.error("Error inserting RSVP:", error);
     return { success: false, message: "Failed to submit RSVP", error };
+  }
+
+  // Send email notification
+  if (!strings.sendToEmail) {
+    console.error("No email to send to");
+    return { success: false, message: "No email to send to" };
+  }
+  if (!error) {
+    try {
+      await resend.emails.send({
+        from: "RSVP <onboarding@resend.dev>",
+        to: strings.sendToEmail,
+        subject: "New RSVP Submission",
+        html: `
+        <h1>New RSVP Submission</h1>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Number of Guests:</strong> ${accompany}</p>
+        <p><strong>Attendance:</strong> ${attendance}</p>
+      `,
+      });
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
   }
 
   return { success: true, message: "RSVP submitted successfully!" };
